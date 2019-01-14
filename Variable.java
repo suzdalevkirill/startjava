1aa2b2920488ce94734687ca9afd2bddb60b5f21
suzdalevkirill <suzdalev.kirill@gmail.com>
2019-01-14 23:26:14 +0300

    create

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+	public static void main(String[] args) {
+       
+	  int vInt = 4;
+	  short vShort = 64;
+	  long vLong = 4308;
+	  float vFloat = 2.8f;
+	  double vDouble = 2.8;
+	  double d = 10.10;
+	  char vChar = 'i';
+	  int  vI = 5;
+	  boolean vBoolean = true;
+	  System.out.println("vInt "+ vInt); 
+	  System.out.println("vShort " + vShort); 
+	  System.out.println("vLong " + vLong); 
+	  System.out.println("vFloat " + vFloat); 
+	  System.out.println("vDouble " + vDouble); 
+	  System.out.println("vChar " + vChar + vI); 
+	  System.out.println("vBoolean " + vBoolean);
+	}
+}
